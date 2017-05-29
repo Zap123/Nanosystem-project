174fa5e70c37b0012cc357f4a427aeddb6bb9e8c
Luca Costa <zapduke@gmail.com>
2017-05-29 18:21:14 +0200

    First Try with Networking

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -21,4 +22,30 @@ void MainWindow::on_actionCalibrate_triggered()
 void MainWindow::on_actionMeasure_triggered()
 {
     emit(changeState("MEASURE"));
+}
+
+void MainWindow::displayError(QAbstractSocket::SocketError socketError){
+    qDebug() << "NETWORK ERROR:";
+    qDebug() << socketError;
+}
+
+void MainWindow::sessionOpened(){
+    qDebug() << "Session Opened";
+}
+
+void MainWindow::on_actionConnect_triggered()
+{
+    //Open Network Interface
+    QNetworkConfigurationManager manager;
+    QNetworkConfiguration config = manager.defaultConfiguration();
+    QNetworkSession *networkSession = new QNetworkSession(config, this);
+    networkSession->open();
+
+
+    QTcpSocket *tcpSocket = new QTcpSocket(this);
+    tcpSocket->connectToHost(ipAddress, port);
+
+    connect(tcpSocket, SIGNAL(error(QAbstractSocket::SocketError)),
+            this, SLOT(displayError(QAbstractSocket::SocketError)));
+    connect(networkSession, SIGNAL(opened()), this, SLOT(sessionOpened()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 #include "programcontext.h"
+#include <QAbstractSocket>
+#include <QTcpSocket>
+#include <QNetworkSession>
+#include <QNetworkConfiguration>
+#include <QNetworkConfigurationManager>
+#include <QNetworkSession>
 
 namespace Ui {
 class MainWindow;
@@ -15,11 +21,18 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    const QString ipAddress = "192.168.1.21";
+    const int port = 5040;
+
 
 private slots:
     void on_actionCalibrate_triggered();
-
+    void displayError(QAbstractSocket::SocketError socketError);
     void on_actionMeasure_triggered();
+    void sessionOpened();
+
+
+    void on_actionConnect_triggered();
 
 signals:
     //state you want to move
